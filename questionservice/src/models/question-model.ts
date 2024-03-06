993495325929a473dad69e90bbb9383d9ffcdce9
Raulms29 <UO287827@uniovi.es>
2024-03-06 20:42:04 +0100

    Added more question templates.

--- questionservice/src/models/question-model.ts
+++ questionservice/src/models/question-model.ts
@@ -33,7 +33,7 @@ const generateSampleTest = async () => {
   // -------- TEST!! REMOVE THIS DATA SAMPLE....WE NEED ANOTHER WAY TO STORE 
   // TEMPLATES UPON CONSTRUCTION
 
-  // Template for capital of a country
+  // Capital of a place
   let aQuestion = new QuestionModel({
     questionTemplate: "What is the Capital of $$$ ?", // $$$ is a placeholder, we will substitute it with the country name
     question_type: {
@@ -54,6 +54,7 @@ const generateSampleTest = async () => {
 
   aQuestion.save()
 
+  // Population of a place
   aQuestion = new QuestionModel({
     questionTemplate: "What is the Population of $$$?",
     question_type: {
@@ -68,11 +69,119 @@ const generateSampleTest = async () => {
             LIMIT 10`,
       question_type_entities: ["Q6256" // Country (any)
         , "Q10742" // Autonomous Community of Spain
-        , "Q35657"] // State of the United States
+        , "Q35657" // State of the United States
+        , "Q515"] // City
     }
   });
 
   aQuestion.save()
+
+  // Year on which a city,country,etc was founded
+  aQuestion = new QuestionModel({
+    questionTemplate: "On which year was $$$ founded?",
+    question_type: {
+      name: "Foundation",
+      query: `SELECT DISTINCT ?templateLabel ?answerLabel
+      WHERE {
+        ?template wdt:P31 wd:$$$; # Entity
+                 wdt:P571 ?answer;
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en"}.
+        BIND(YEAR(?answer) AS ?answerLabel)
+      
+      }
+      ORDER BY UUID()
+      LIMIT 10
+      `,
+      question_type_entities: ["Q6256" // Country (any)
+        , "Q10742" // Autonomous Community of Spain
+        , "Q35657" // State of the United States
+        , "Q6881511" // Company
+        , "Q515"] // City
+    }
+  });
+
+  aQuestion.save();
+
+
+  // Year in which an event occurred
+  aQuestion = new QuestionModel({
+    questionTemplate: "On which year did the $$$ took place?",
+    question_type: {
+      name: "Events",
+      query: `SELECT DISTINCT ?templateLabel ?answerLabel
+      WHERE {
+        ?template wdt:P31 wd:$$$; # Entity
+                 wdt:P571 ?answer;
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en"}.
+        BIND(YEAR(?answer) AS ?answerLabel)
+      }
+      ORDER BY UUID()
+      LIMIT 10
+      `,
+      question_type_entities: ["Q1190554" // Any event (maybe too general)
+        , "Q6107280" // Revolt
+        , "Q198" // War
+        , "Q209715"] // Coronation of a king/queen
+    }
+  });
+
+
+  // Who wrote...?
+  aQuestion = new QuestionModel({
+    questionTemplate: "Who wrote $$$?",
+    question_type: {
+      name: "Events",
+      query: `SELECT ?templateLabel ?answerLabel
+      WHERE {
+        ?template wdt:P106 wd:Q36180; 
+                wdt:P800 ?answer.
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en,es". }
+      }
+      ORDER BY UUID()
+      LIMIT 10
+      `,
+    }
+  });
+
+  aQuestion.save();
+
+  // Chemical symbol of an element
+  // We make a first query searching for any element in the periodic table
+  // Then we search for elements with similar associated symbols instead of selecting at random
+  // on the results to make the question not so easy
+  aQuestion = new QuestionModel({
+    questionTemplate: "What is the chemical symbol of $$$?",
+    question_type: {
+      name: "Chemistry",
+      query: `SELECT ?templateLabel ?answerLabel
+      WHERE
+      {
+        ?template wdt:P31 wd:Q11344;
+                 wdt:P246 ?answer;
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+      }
+      `
+    }
+  });
+
+  // Atomic number of an element
+  // Here it's not necessary to increase difficulty, it's hard enough.
+  aQuestion = new QuestionModel({
+    questionTemplate: "What is the the atomic number of $$$?",
+    question_type: {
+      name: "Chemistry",
+      query: `SELECT ?templateLabel ?answerLabel
+      WHERE
+      {
+        ?template wdt:P31 wd:Q11344;
+                 wdt:P1086 ?answer;
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+      }
+      `
+    }
+  });
+  aQuestion.save();
+
 
   // Raúl
   // With this query I can consistently get several tourists attractions/famous places from a country.
@@ -91,43 +200,44 @@ const generateSampleTest = async () => {
   // IMPORTANT: Probably there is a better approach for the query below, but with mi current knowledge of 
   // the Wikidata Query Service, I don't know which it's.
 
-  aQuestion = new QuestionModel({
-    questionTemplate: "What is a famous place from Peru?",
-    question_type: {
-      name: "Landmarks",
-      query: `SELECT ?templateLabel ?answerLabel
-              WHERE {
+  // aQuestion = new QuestionModel({
+  //   questionTemplate: "What is a famous place from Peru?",
+  //   question_type: {
+  //     name: "Landmarks",
+  //     query: `SELECT ?templateLabel ?answerLabel
+  //             WHERE {
 
-                ?template wdt:P31/wdt:P279* wd:Q4989906;
-                wdt:P17  ?answer.
-                
-                SERVICE wikibase:label { bd:serviceParam wikibase:language "en,es"}.
-                
-                {
-                  SELECT ?answer
-                  WHERE {
-                    ?answer wdt:P31 wd:Q6256; # Entity
-                  }
-                  ORDER BY UUID()
-                  LIMIT 1
-                }
-              }
-              ORDER BY UUID() # Add randomness to the results
-              LIMIT 2`
-    }
-  });
+  //               ?template wdt:P31/wdt:P279* wd:Q4989906;
+  //               wdt:P17  ?answer.
 
-  aQuestion.save()
+  //               SERVICE wikibase:label { bd:serviceParam wikibase:language "en,es"}.
 
-  aQuestion = new QuestionModel({
-    questionTemplate: "What is the typical dish of Peru?",
-    question_type: {
-      name: "Gastronomy",
-      query: "DELETE ALL"
-    }
-  });
+  //               {
+  //                 SELECT ?answer
+  //                 WHERE {
+  //                   ?answer wdt:P31 wd:Q6256; # Entity
+  //                 }
+  //                 ORDER BY UUID()
+  //                 LIMIT 1
+  //               }
+  //             }
+  //             ORDER BY UUID() # Add randomness to the results
+  //             LIMIT 2`
+  //   }
+  // });
 
-  aQuestion.save()
+  // aQuestion.save()
+
+  // aQuestion = new QuestionModel({
+  //   questionTemplate: "What is the typical dish of Peru?",
+  //   question_type: {
+  //     name: "Gastronomy",
+  //     query: "DELETE ALL"
+  //   }
+  // });
+
+  // aQuestion.save()
+
 }
 
 export { QuestionModel, generateSampleTest };

3735fd2569e56a06abffa217e483d05e6924fc9c
Iyan Robles <UO288780@uniovi.es>
2024-03-08 09:47:49 +0100

    Added more tests to maximize coverage

--- users/userservice/test/user-service.test.ts
+++ users/userservice/test/user-service.test.ts
@@ -32,8 +32,27 @@ describe('User Service', () => {
     expect(response.body.data.user).toHaveProperty('username', 'testuser');
   });
 
-  // GET /history error
-  it('should get an error in /history when the user does not exist', async () => {
+  it('should get an error for duplicated user on POST /adduser', async () => {
+    const newUser = {
+      username: 'testuser',
+      password: 'testpassword2',
+    };
+
+    const response = await request(app).post('/adduser').send(newUser);
+    expect(response.status).toBe(400);
+  });
+
+  // GET /history error (no user)
+  it('should get an error in GET /history when not passing a user', async () => {
+    const response = await request(app)
+        .get('/history');
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.data.history).toBeUndefined();
+  });
+
+  // GET /history error (non-existent user)
+  it('should get an error in GET /history when user does not exist', async () => {
     const response = await request(app)
         .get('/history?user=nonexistentuser');
 
@@ -42,7 +61,7 @@ describe('User Service', () => {
   });
 
   // GET /history success
-  it('should obtain the history data for the user', async () => {
+  it('should obtain the history data for the user with GET /history', async () => {
     const response = await request(app)
         .get('/history?user=testuser');
 
@@ -52,9 +71,29 @@ describe('User Service', () => {
     expectedProperties.forEach(property => expect(response.body.data.history).toHaveProperty(property));
   });
 
+  // POST /history (no user)
+  it('should get an error in POST /history when not logged in', async () => {
+    const newHistory = {
+      history: {
+        passedQuestions: 0,
+        failedQuestions: 0,
+        gamesPlayed: 0,
+        timePlayed: 0,
+        points: 0,
+      },
+    };
+
+    const response = await request(app)
+        .post('/history')
+        .send(newHistory);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.data.history).toBeUndefined();
+  });
+
   let testToken = '';
   // POST /history
-  it('should update the history data for the user', async () => {
+  it('should update the history data for the user with POST /history', async () => {
     const user = await User.findOne({ username:'testuser' });
 
     // Generates a temporary token for this and other tests
@@ -86,8 +125,44 @@ describe('User Service', () => {
     expect(JSON.stringify(getResponse.body.data.history)).toMatch(JSON.stringify(newHistory.history));
   });
 
-  // POST /history/increment
-  it('should increment the value for the given key in the history', async () => {
+  // POST /history (no history)
+  it('should get an error in POST /history when not sending a new history', async () => {
+    const response = await request(app)
+        .post('/history')
+        .set('Authorization', `Bearer ${testToken}`);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.data.history).toBeUndefined();
+  });
+
+  // POST /history/increment (no user)
+  it('should get an error in POST /history/increment when not logged in', async () => {
+    const increment = {
+      history: {
+        passedQuestions: 1,
+      },
+    };
+
+    const response = await request(app)
+        .post('/history/increment')
+        .send(increment);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.data.history).toBeUndefined();
+  });
+
+  // POST /history (no increment)
+  it('should get an error in POST /history/increment when not sending a json with increments', async () => {
+    const response = await request(app)
+        .post('/history/increment')
+        .set('Authorization', `Bearer ${testToken}`);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.data.history).toBeUndefined();
+  });
+
+  // POST /history/increment success
+  it('should increment the values of history with POST /history/increment', async () => {
     const expectedHistory = {
       history: {
         passedQuestions: 12,

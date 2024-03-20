654859c549780000eeeb24548302f7d702097ea3
Raulms29 <UO287827@uniovi.es>
2024-03-20 19:55:07 +0100

    Refactor and Documentation of question-generator

--- questionservice/src/services/question-generator.ts
+++ questionservice/src/services/question-generator.ts
@@ -1,13 +1,81 @@
 import { QuestionModel } from '../models/question-model';
 import { getWikidataSparql } from '@entitree/helper';
 
-// Gets a random Item from an array
+const distractorsNumber: number = 3;
+const optionsNumber: number = distractorsNumber + 1;
+
+/**
+ * Generates n random questions  using Wikidata
+ * @param n number of questions to be generated
+ * @returns array containing questions and possible answers
+ */
+async function generateQuestions(n: number): Promise<object[] | void> {
+  try {
+    // Obtain n random documents
+    const randomQuestionsTemplates = await QuestionModel.aggregate([
+      { $sample: { size: n } },
+    ]);
+
+    // Generate and return questions generated from those documents
+    const questionsArray = await generateQuestionsArray(
+      randomQuestionsTemplates
+    );
+    return questionsArray;
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * Generates an array of questions given a set of templates
+ * @param randomQuestionsTemplates Array of templates
+ * @returns array containing questions in JSON format
+ */
+const generateQuestionsArray = async (
+  randomQuestionsTemplates: any
+): Promise<object[]> => {
+  // Simply a fx to add the JSON questions to the Array
+
+  // For each questionTemplate, we generate an async func to generate the questions
+  // and its answers
+  const promises = randomQuestionsTemplates.map(
+    async (template: any, i: number) => {
+      try {
+        console.log('Generating question of type ' + template.question_type.name);
+        const questionJson = await generateQuestionJson(template, i);
+        console.log('Question generated!');
+
+        return questionJson;
+      } catch (error) {
+        console.error(
+          'Error while generating question for template: ' + template
+        );
+        throw error;
+      }
+    }
+  );
+
+  return Promise.all(promises);
+};
+
+
+/**
+ * Gets a random item from an array
+ * @param array 
+ * @returns A random item from the array
+ */
 function getRandomItem<T>(array: T[]): T {
   const randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
 }
 
-// Builds a question JSON out of parameters
+/**
+ * Builds a question JSON out of parameters
+ * @param templateNumber number of the template
+ * @param questionGen  Question generated
+ * @param answersArray  Array of answers
+ * @returns the JSON question
+ */
 const questionJsonBuilder = (
   templateNumber: number,
   questionGen: string,
@@ -29,101 +97,85 @@ const generateQuestionJson = async (
   templateNumber: number
 ): Promise<object | void> => {
   try {
-    // Template Query
-    var sparqlQuery: string = document.question_type.query;
 
     // Options may be present...
-    var optionEntities = document.question_type.entities as string[];
-    if (optionEntities.length > 0) {
-      var randomEntity = getRandomItem(optionEntities);
-      sparqlQuery = sparqlQuery.replace(/\$\$\$/g, randomEntity);
-    }
+    let sparqlQuery: string = getSparqlQueryFromDocument(document);
 
-    console.log('Stuck?');
     // Make wikidata request and obtain response
-    const res = await getWikidataSparql(sparqlQuery);
-    console.log('No');
+    const wikidataResponse = await getWikidataSparql(sparqlQuery);
 
     // Pick random responses
-    var randomIndexes: number[] = [];
-    for (var i = 0; i < 4; i++) {
-      var possibleRandom = Math.floor(Math.random() * res.length);
-      while (randomIndexes.includes(possibleRandom))
-        possibleRandom = Math.floor(Math.random() * res.length);
-
-      randomIndexes[i] = possibleRandom;
-    }
+    var randomIndexes: number[] = generateRandomIndexes(wikidataResponse.length);
 
     // Generate question
     var questionGen = document.questionTemplate.replace(
       /\$\$\$/g,
-      res[randomIndexes[0]].templateLabel
+      wikidataResponse[randomIndexes[0]].templateLabel
     );
 
     // Generate answers
-    var answersArray: object[] = [];
-    for (var i = 0; i < 4; i++) {
-      var answer = res[randomIndexes[i]].answerLabel;
-      answersArray[i] = {
-        id: i + 1,
-        text: answer,
-      };
-    }
+    var answersArray: object[] = getRandomResponses(wikidataResponse, randomIndexes);
 
     // Build it
     return questionJsonBuilder(templateNumber, questionGen, answersArray);
   } catch (error) {
+    console.error(error);
     console.error('Error while fetching Wikidata');
     throw error;
   }
 };
 
-// Generate the JSON questions Array
-const generateQuestionsArray = async (
-  randomQuestionsTemplates: any
-): Promise<object[]> => {
-  // Simply a fx to add the JSON questions to the Array
+/**
+ * Generates random indexes for the answers
+ * @param length length of the array
+ * @param numberOfIndexes number of indexes to generate
+ * @returns an array with the indexes
+ */
+function generateRandomIndexes(length: number, numberOfIndexes: number = optionsNumber) {
+  var randomIndexes: number[] = [];
+  for (var i = 0; i < numberOfIndexes; i++) {
+    var possibleRandom = Math.floor(Math.random() * length);
+    while (randomIndexes.includes(possibleRandom))
+      possibleRandom = Math.floor(Math.random() * length);
 
-  // For each questionTemplate, we generate an async func to generate the questions
-  // and its answers
-  const promises = randomQuestionsTemplates.map(
-    async (template: any, i: number) => {
-      try {
-        const questionJson = await generateQuestionJson(template, i);
-
-        return questionJson;
-      } catch (error) {
-        console.error(
-          'Error while generating question for template: ' + template
-        );
-        throw error;
-      }
-    }
-  );
-
-  return Promise.all(promises);
-};
-
-// Principal function in charge of generating the questions
-async function generateQuestions(n: number): Promise<object[] | void> {
-  try {
-    // Generating sample test. TODO: To be removed for next iteration
-
-    // Obtain n random documents
-    const randomQuestionsTemplates = await QuestionModel.aggregate([
-      { $sample: { size: n } },
-    ]);
-    // console.log('124');
-    // console.log(randomQuestionsTemplates[0].question_type.name);
-
-    // Generate and return questions generated from those documents
-    const questionsArray = await generateQuestionsArray(
-      randomQuestionsTemplates
-    );
-    return questionsArray;
-  } catch (error) {
-    throw error;
+    randomIndexes[i] = possibleRandom;
   }
+  return randomIndexes;
+}
+
+/**
+ * Includes the eligible entities on the SPARQL query and returns it
+ * @param sparqlQuery the query
+ * @param document document
+ * @returns the query with the entities included, if possible
+ */
+function getSparqlQueryFromDocument(document: any): string {
+  let sparqlQuery: string = document.question_type.query;
+  let optionEntities = document.question_type.entities as string[];
+  if (optionEntities.length > 0) {
+    var randomEntity = getRandomItem(optionEntities);
+    sparqlQuery = sparqlQuery.replace(/\$\$\$/g, randomEntity);
+  }
+  return sparqlQuery;
+}
+
+
+/**
+ * Generates random responses from the values returned by Wikidata
+ * @param wikidataResponse response from Wikidata
+ * @param randomIndexes array of random indexes
+ * @returns array of answers
+ */
+function getRandomResponses(wikidataResponse: any, randomIndexes: number[]): any {
+  let answersArray: object[] = [];
+  for (var i = 0; i < optionsNumber; i++) {
+    var answer = wikidataResponse[randomIndexes[i]].answerLabel;
+    answersArray[i] = {
+      id: i + 1,
+      text: answer,
+    };
+  }
+  return answersArray;
 }
 
 export { generateQuestions };

5ae9570cd7644701f92dd959d44e48b076014152
Iyan Robles <UO288780@uniovi.es>
2024-03-26 01:45:58 +0100

    Deleted source maps and changed jest configuration

--- users/utils/jest.config.js
+++ users/utils/jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  coverageReporters: [
+    ["lcov", {"projectRoot": "../"}],
+    "text-summary"
+  ]
 };

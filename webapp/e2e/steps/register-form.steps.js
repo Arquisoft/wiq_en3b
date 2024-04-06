b5003ebaabc1784b1159f7d803e1febaf42b13f9
carlosmndzg <cjr2344@gmail.com>
2024-04-06 09:07:35 +0200

    Added one registered test and refactored steps

--- webapp/e2e/steps/register-form.steps.js
+++ webapp/e2e/steps/register-form.steps.js
@@ -2,6 +2,12 @@ const puppeteer = require('puppeteer');
 const { defineFeature, loadFeature }=require('jest-cucumber');
 const setDefaultOptions = require('expect-puppeteer').setDefaultOptions
 const feature = loadFeature('./features/register-form.feature');
+const { 
+    waitForWelcomeMessage, 
+    switchFromLoginPageToRegisterPageByUsingLink,
+    registerUser,
+    waitForUserAlreadyRegisteredError 
+} = require('./register-form.utils')
 
 let page;
 let browser;
@@ -29,21 +35,49 @@ defineFeature(feature, test => {
     let password;
 
     given('An unregistered user', async () => {
-      username = "kawuser"
-      password = "kawpass123@"
-      await expect(page).toClick("a", { text: "Not yet registered? Register Now" });
+        username = "kawuser"
+        password = "kawpass123@"
+        await switchFromLoginPageToRegisterPageByUsingLink(page)
     });
 
     when('I fill the data in the form and press submit', async () => {
-      await expect(page).toFill('input[name="username"]', username);
-      await expect(page).toFill('input[name="password"]', password);
-      await expect(page).toFill('input[name="repeatPassword"]', password);
-      await expect(page).toClick('button', { text: 'Register' })
+        await registerUser(page, username, password);
     });
 
     then('A confirmation message should be shown in the screen', async () => {
-        await expect(page).toMatchElement('.welcome-message', { text: 'Welcome back,' });
-        await expect(page).toMatchElement('.welcome-message .username', { text: username });
+        await waitForWelcomeMessage(page, username)
+    });
+  })
+
+  afterEach(async () => {
+    await page.evaluate(() => {
+      localStorage.clear();
+    });
+
+    await page
+      .goto("http://localhost:3000", {
+        waitUntil: "networkidle0",
+      })
+      .catch(() => {});
+  });
+
+  test('The user is already registered in the site', ({given,when,then}) => {
+    
+    let username;
+    let password;
+
+    given('An already registered user', async () => {
+        username = "kawuser"
+        password = "kawpass123@"
+        await switchFromLoginPageToRegisterPageByUsingLink(page)
+    });
+
+    when('I fill the data in the form and press submit', async () => {
+        await registerUser(page, username, password);
+    });
+
+    then('An error message should be shown in the screen', async () => {
+        await waitForUserAlreadyRegisteredError(page, username)
     });
   })
 

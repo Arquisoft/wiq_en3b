22cd2a7ee4c3701afd2c22e3c2a88652539b80fc
Iyan Robles <UO288780@uniovi.es>
2024-04-10 16:53:27 +0200

    Refactoring for consistency issues

--- gatewayservice/src/utils/constants.ts
+++ gatewayservice/src/utils/constants.ts
@@ -1,11 +1,11 @@
 const AUTH_SERVICE_URL =
-  process.env.AUTH_SERVICE_URL || 'http://localhost:8002';
+  process.env.AUTH_SERVICE_URL ?? 'http://localhost:8002';
 const USER_SERVICE_URL =
-  process.env.USER_SERVICE_URL || 'http://localhost:8001';
+  process.env.USER_SERVICE_URL ?? 'http://localhost:8001';
 const HISTORY_SERVICE_URL =
-  process.env.HISTORY_SERVICE_URL || 'http://localhost:8001';
+  process.env.HISTORY_SERVICE_URL ?? 'http://localhost:8001';
 const QUESTION_SERVICE_URL =
-  process.env.QUESTION_SERVICE_URL || 'http://localhost:8004';
+  process.env.QUESTION_SERVICE_URL ?? 'http://localhost:8004';
 
 export {
   AUTH_SERVICE_URL,

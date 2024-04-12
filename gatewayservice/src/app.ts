37d703eb19d7b88fd755af8f6903c8aeeb2b5957
Raulms29 <UO287827@uniovi.es>
2024-04-12 12:07:50 +0200

    Fixed CORS for kawgame.xyz

--- gatewayservice/src/app.ts
+++ gatewayservice/src/app.ts
@@ -20,7 +20,7 @@ app.disable("x-powered-by");
 const frontURI = process.env.WEBAPP_ENDPOINT ?? 'http://localhost:3000'
 const gatewayURI = process.env.GATEWAY_ENDPOINT ?? 'http://localhost:8000'
 
-const corsWhitelist = [frontURI, gatewayURI, 'http://localhost', 'http://kawgame.xyz:80', 'http://kawgame.xyz:8000']
+const corsWhitelist = [frontURI, gatewayURI, 'http://localhost', 'http://kawgame.xyz', 'http://kawgame.xyz:8000']
 app.use(cors({ origin: corsWhitelist }));
 app.use(express.json());
 

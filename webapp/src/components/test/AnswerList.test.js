285ce6dcb32610fdb5b849504c674e8906fee8f7
carlosmndzg <cjr2344@gmail.com>
2024-04-12 18:08:42 +0200

    Updated tests

--- webapp/src/components/test/AnswerList.test.js
+++ webapp/src/components/test/AnswerList.test.js
@@ -1,32 +1,57 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import AnswerList from '../Answer/AnswerList';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import AnswerList from '../Answer/AnswerList'
+
+const CORRECT_ANSWER_ID = 2
+const INCORRECT_ANSWER_ID = 1
+const NO_ANSWER_ID = 0
+
+const answersData = {
+  correctAnswerId: CORRECT_ANSWER_ID,
+  answers: [
+    { id: 1, text: 'Answer 1' },
+    { id: 2, text: 'Answer 2' },
+    { id: 3, text: 'Answer 3' },
+    { id: 4, text: 'Answer 4' },
+  ],
+}
 
 describe('AnswerList component', () => {
-  it('calls onSelectAnswer with the correct answer ID when an answer is clicked', () => {
-    // Arrange
-    const answers = [
-      { id: 'answer1', text: 'Answer 1' },
-      { id: 'answer2', text: 'Answer 2' },
-      { id: 'answer3', text: 'Answer 3' },
-      { id: 'answer4', text: 'Answer 4' },
-    ];
-    const onSelectAnswerMock = jest.fn();
-    const selectedAnswerId = 'answer2'; // Set the selected answer ID
-
-    // Act
+  it('renders successfully AnswerList', () => {
     render(
-        <AnswerList
-            answers={answers}
-            onSelectAnswer={onSelectAnswerMock}
-            selected={selectedAnswerId}
-            btnDisabled={false}
-        />
-    );
-    const selectedAnswer = screen.getByText('Answer 2');
-    fireEvent.click(selectedAnswer);
+      <AnswerList
+        answersData={answersData}
+        answerQuestionWith={_ => {}}
+        selectedAnswerId={NO_ANSWER_ID}
+      />
+    )
 
-    // Assert
-    expect(onSelectAnswerMock).toHaveBeenCalledWith('answer2');
-  });
-});
+    expect(screen.getByText('Answer 2')).toBeTruthy()
+  })
+
+  it('renders AnswerList with only one correct answer when user guesses', () => {
+    render(
+      <AnswerList
+        answersData={answersData}
+        answerQuestionWith={_ => {}}
+        selectedAnswerId={CORRECT_ANSWER_ID}
+      />
+    )
+
+    expect(screen.getByText('Answer 2')).toHaveClass('success')
+  })
+
+  it('renders AnswerList with only one correct answer and one incorrect when user is wrong', () => {
+    render(
+      <AnswerList
+        answersData={answersData}
+        answerQuestionWith={_ => {}}
+        selectedAnswerId={INCORRECT_ANSWER_ID}
+      />
+    )
+
+    expect(screen.getByText('Answer 2')).toHaveClass('success')
+    expect(screen.getByText('Answer 1')).toHaveClass('error')
+  })
+})
--- webapp/src/components/test/Game.test.js
+++ webapp/src/components/test/Game.test.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import { I18nextProvider } from 'react-i18next';
-import i18n from 'i18next'; // Importa i18next directamente
-import Game from '../../pages/Game/Game';
-import { AuthContext } from '../../context/AuthContext'; // Asegúrate de importar AuthContext desde la ubicación correcta
+import React from 'react'
+import { render, fireEvent, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { I18nextProvider } from 'react-i18next'
+import i18n from 'i18next' // Importa i18next directamente
+import Game from '../../pages/Game/Game'
+import { AuthContext } from '../../context/AuthContext' // Asegúrate de importar AuthContext desde la ubicación correcta
 
 // Crea un mock de i18next
 i18n.init({
@@ -20,38 +20,38 @@ i18n.init({
       },
     },
   },
-});
+})
 
 // Define un usuario mock
 const mockUser = {
   // Proporciona aquí los datos del usuario que necesitas para tus pruebas
-};
+}
 
 test('renders difficulty selection when no level is selected', async () => {
-    const { findByText } = render(
-        <AuthContext.Provider value={{ user: mockUser }}>
-            <I18nextProvider i18n={i18n}>
-                <Game />
-            </I18nextProvider>
-        </AuthContext.Provider>
-    );
-    expect(await findByText('Choose difficulty')).toBeInTheDocument();
-    expect(await findByText('Easy')).toBeInTheDocument();
-    expect(await findByText('Medium')).toBeInTheDocument();
-    expect(await findByText('Hard')).toBeInTheDocument();
-});
+  render(
+    <AuthContext.Provider value={{ user: mockUser }}>
+      <I18nextProvider i18n={i18n}>
+        <Game />
+      </I18nextProvider>
+    </AuthContext.Provider>
+  )
+  expect(await screen.findByText('Choose difficulty')).toBeInTheDocument()
+  expect(await screen.findByText('Easy')).toBeInTheDocument()
+  expect(await screen.findByText('Medium')).toBeInTheDocument()
+  expect(await screen.findByText('Hard')).toBeInTheDocument()
+})
 
 test('difficulty selection disappears when a level is selected', async () => {
-    const { findByText, queryByText } = render(
-        <AuthContext.Provider value={{ user: mockUser }}>
-            <I18nextProvider i18n={i18n}>
-                <Game />
-            </I18nextProvider>
-        </AuthContext.Provider>
-    );
-    fireEvent.click(await findByText('Easy'));
-    expect(queryByText('Choose difficulty')).not.toBeInTheDocument();
-    expect(queryByText('Easy')).not.toBeInTheDocument();
-    expect(queryByText('Medium')).not.toBeInTheDocument();
-    expect(queryByText('Hard')).not.toBeInTheDocument();
-});
+  render(
+    <AuthContext.Provider value={{ user: mockUser }}>
+      <I18nextProvider i18n={i18n}>
+        <Game />
+      </I18nextProvider>
+    </AuthContext.Provider>
+  )
+  fireEvent.click(await screen.findByText('Easy'))
+  expect(screen.queryByText('Choose difficulty')).not.toBeInTheDocument()
+  expect(screen.queryByText('Easy')).not.toBeInTheDocument()
+  expect(screen.queryByText('Medium')).not.toBeInTheDocument()
+  expect(screen.queryByText('Hard')).not.toBeInTheDocument()
+})

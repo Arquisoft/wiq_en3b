ba67ed70fe95fb16a2b20a05a1d7a66e9e59ea23
Raulms29 <UO287827@uniovi.es>
2024-04-21 18:24:52 +0200

    Added 2 more Templates

--- questionservice/src/models/template-model.ts
+++ questionservice/src/models/template-model.ts
@@ -40,208 +40,208 @@ const addQuestionTemplate = (questionTemplate: any) => {
 const generateSampleTest = () => {
 
   // Capital of a place
-  // addQuestionTemplate({
-  //   questionTemplate: 'What is the Capital of $$$ ?', // $$$ is a placeholder, we will substitute it with the country name
-  //   question_type: {
-  //     name: 'Capitals',
-  //     query: `SELECT ?templateLabel ?answerLabel
-  //             WHERE {
-  //               ?template wdt:P31 wd:$$$; # Entity
-  //                       wdt:P36 ?answer.  # Capital
-  //               SERVICE wikibase:label { bd:serviceParam wikibase:language "en,es"}
-  //             }
-  //             ORDER BY UUID() # Add randomness to the results
-  //             LIMIT 10`,
-  //     entities: [
-  //       'Q6256', // Country (any)
-  //       'Q10742', // Autonomous Community of Spain
-  //     ],
-  //     typeName: 'geography',
-  //   },
-  // });
+  addQuestionTemplate({
+    questionTemplate: 'What is the Capital of $$$ ?', // $$$ is a placeholder, we will substitute it with the country name
+    question_type: {
+      name: 'Capitals',
+      query: `SELECT ?templateLabel ?answerLabel
+              WHERE {
+                ?template wdt:P31 wd:$$$; # Entity
+                        wdt:P36 ?answer.  # Capital
+                SERVICE wikibase:label { bd:serviceParam wikibase:language "en,es"}
+              }
+              ORDER BY UUID() # Add randomness to the results
+              LIMIT 10`,
+      entities: [
+        'Q6256', // Country (any)
+        'Q10742', // Autonomous Community of Spain
+      ],
+      typeName: 'geography',
+    },
+  });
 
 
-  // // Population of a place
-  // addQuestionTemplate({
-  //   questionTemplate: 'What is the Population of $$$?',
-  //   question_type: {
-  //     name: 'Demography',
-  //     query: `SELECT ?templateLabel ?answerLabel
-  //             WHERE {
-  //               ?template wdt:P31 wd:$$$;  # Entity
-  //                       wdt:P1082 ?answer. # Population
-  //               SERVICE wikibase:label { bd:serviceParam wikibase:language "en,es"}
-  //             }
-  //             ORDER BY UUID() # Add randomness to the results
-  //             LIMIT 10`,
-  //     entities: [
-  //       'Q6256', // Country (any)
-  //       'Q10742', // Autonomous Community of Spain
-  //       'Q35657', // State of the United States
-  //     ],
-  //     typeName: 'geography',
-  //   },
-  // });
+  // Population of a place
+  addQuestionTemplate({
+    questionTemplate: 'What is the Population of $$$?',
+    question_type: {
+      name: 'Demography',
+      query: `SELECT ?templateLabel ?answerLabel
+              WHERE {
+                ?template wdt:P31 wd:$$$;  # Entity
+                        wdt:P1082 ?answer. # Population
+                SERVICE wikibase:label { bd:serviceParam wikibase:language "en,es"}
+              }
+              ORDER BY UUID() # Add randomness to the results
+              LIMIT 10`,
+      entities: [
+        'Q6256', // Country (any)
+        'Q10742', // Autonomous Community of Spain
+        'Q35657', // State of the United States
+      ],
+      typeName: 'geography',
+    },
+  });
 
 
-  // //   // Year on which a city,country,etc was founded
-  // addQuestionTemplate({
-  //   questionTemplate: 'On which year was $$$ founded?',
-  //   question_type: {
-  //     name: 'Foundation',
-  //     query: `SELECT DISTINCT ?templateLabel ?answerLabel
-  //       WHERE {
-  //         ?template wdt:P31 wd:$$$; # Entity
-  //                  wdt:P571 ?answer;
-  //         SERVICE wikibase:label { bd:serviceParam wikibase:language "en"}.
-  //         BIND(YEAR(?answer) AS ?answerLabel)
+  //   // Year on which a city,country,etc was founded
+  addQuestionTemplate({
+    questionTemplate: 'On which year was $$$ founded?',
+    question_type: {
+      name: 'Foundation',
+      query: `SELECT DISTINCT ?templateLabel ?answerLabel
+        WHERE {
+          ?template wdt:P31 wd:$$$; # Entity
+                   wdt:P571 ?answer;
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "en"}.
+          BIND(YEAR(?answer) AS ?answerLabel)
 
-  //       }
-  //       ORDER BY UUID()
-  //       LIMIT 10
-  //       `,
-  //     entities: [
-  //       'Q6256', // Country (any)
-  //       'Q10742', // Autonomous Community of Spain
-  //       'Q35657', // State of the United States
-  //     ],
-  //     typeName: 'history',
-  //   },
-  // });
-
-
-  // //   // Year in which an event occurred
-  // addQuestionTemplate({
-  //   questionTemplate: 'On which year did the $$$ took place?',
-  //   question_type: {
-  //     name: 'Events',
-  //     query: `SELECT DISTINCT ?templateLabel ?answerLabel
-  //         WHERE {
-  //           ?template wdt:P31 wd:$$$; # Entity
-  //                    wdt:P571 ?answer;
-  //           SERVICE wikibase:label { bd:serviceParam wikibase:language "en,es"}.
-  //           BIND(YEAR(?answer) AS ?answerLabel)
-  //         }
-  //         ORDER BY UUID()
-  //         LIMIT 10
-  //         `,
-  //     entities: [
-  //       'Q198' // War
-  //     ],
-  //     typeName: 'history',
-  //   },
-  // });
-
-  // // Chemical symbol of an element
-  // // We make a first query searching for any element in the periodic table
-  // // Then we search for elements with similar associated symbols instead of selecting at random
-  // // on the results to make the question not so easy
-  // addQuestionTemplate({
-  //   questionTemplate: 'What is the chemical symbol of $$$?',
-  //   question_type: {
-  //     name: 'Chemistry',
-  //     query: `SELECT ?templateLabel ?answerLabel
-  //       WHERE
-  //       {
-  //         ?template wdt:P31 wd:Q11344;
-  //                  wdt:P246 ?answer;
-  //         SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-  //       }
-  //       ORDER BY UUID()
-  //       LIMIT 10
-  //       `,
-  //     entities: [],
-  //     typeName: 'science',
-  //   },
-  // });
+        }
+        ORDER BY UUID()
+        LIMIT 10
+        `,
+      entities: [
+        'Q6256', // Country (any)
+        'Q10742', // Autonomous Community of Spain
+        'Q35657', // State of the United States
+      ],
+      typeName: 'history',
+    },
+  });
 
 
-  // // Atomic number of an element
-  // // Here it's not necessary to increase difficulty, it's hard enough.
-  // addQuestionTemplate({
-  //   questionTemplate: 'What is the the atomic number of $$$?',
-  //   question_type: {
-  //     name: 'Chemistry',
-  //     query: `SELECT ?templateLabel ?answerLabel
-  //     WHERE
-  //     {
-  //       ?template wdt:P31 wd:Q11344;
-  //                wdt:P1086 ?answer;
-  //       SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-  //     }
-  //     ORDER BY UUID()
-  //     LIMIT 10
-  //     `,
-  //     entities: [],
-  //     typeName: 'science',
-  //   },
-  // });
+  //   // Year in which an event occurred
+  addQuestionTemplate({
+    questionTemplate: 'On which year did the $$$ took place?',
+    question_type: {
+      name: 'Events',
+      query: `SELECT DISTINCT ?templateLabel ?answerLabel
+          WHERE {
+            ?template wdt:P31 wd:$$$; # Entity
+                     wdt:P571 ?answer;
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "en,es"}.
+            BIND(YEAR(?answer) AS ?answerLabel)
+          }
+          ORDER BY UUID()
+          LIMIT 10
+          `,
+      entities: [
+        'Q198' // War
+      ],
+      typeName: 'history',
+    },
+  });
 
-  // // Image Question templates
-
-  // // Flag of a country, autonomous community of Spain or USA state
-  // addQuestionTemplate({
-  //   questionTemplate: 'This flag is from...?',
-  //   question_type: {
-  //     name: 'Images_Flags',
-  //     query: `SELECT ?templateLabel ?answerLabel
-  //     WHERE {
-  //       ?answer wdt:P31 wd:$$$; # Entity
-  //               wdt:P41 ?template.  # Capital
-  //       SERVICE wikibase:label { bd:serviceParam wikibase:language "en"}
-  //     }
-  //     ORDER BY UUID() # Add randomness to the results
-  //     LIMIT 5
-  //     `,
-  //     entities: [
-  //       'Q6256', // Country (any)
-  //       'Q10742', // Autonomous Community of Spain
-  //       'Q35657' // State of the United States],
-  //     ],
-  //     typeName: 'geography',
-  //   },
-  // });
+  // Chemical symbol of an element
+  // We make a first query searching for any element in the periodic table
+  // Then we search for elements with similar associated symbols instead of selecting at random
+  // on the results to make the question not so easy
+  addQuestionTemplate({
+    questionTemplate: 'What is the chemical symbol of $$$?',
+    question_type: {
+      name: 'Chemistry',
+      query: `SELECT ?templateLabel ?answerLabel
+        WHERE
+        {
+          ?template wdt:P31 wd:Q11344;
+                   wdt:P246 ?answer;
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+        }
+        ORDER BY UUID()
+        LIMIT 10
+        `,
+      entities: [],
+      typeName: 'science',
+    },
+  });
 
 
-  // // Guess the person by an image
-  // // In this case physiscists
-  // addQuestionTemplate({
-  //   questionTemplate: 'Who is this physiscist?',
-  //   question_type: {
-  //     name: 'Images_Physics',
-  //     query: `SELECT ?templateLabel ?answerLabel
-  //     WHERE {
-  //       ?answer wdt:P31 wd:Q5;  # Instance of human
-  //               wdt:P106 wd:Q169470;  # Occupation: "physicist"
-  //               wdt:P18 ?template;
-  //       SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-  //     }
-  //     LIMIT 5    
-  //     `,
-  //     entities: [],
-  //     typeName: 'science',
-  //   },
-  // });
+  // Atomic number of an element
+  // Here it's not necessary to increase difficulty, it's hard enough.
+  addQuestionTemplate({
+    questionTemplate: 'What is the the atomic number of $$$?',
+    question_type: {
+      name: 'Chemistry',
+      query: `SELECT ?templateLabel ?answerLabel
+      WHERE
+      {
+        ?template wdt:P31 wd:Q11344;
+                 wdt:P1086 ?answer;
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+      }
+      ORDER BY UUID()
+      LIMIT 10
+      `,
+      entities: [],
+      typeName: 'science',
+    },
+  });
 
-  // // Guess the person by an image
-  // // In this case inventors
-  // addQuestionTemplate({
-  //   questionTemplate: 'Who is this inventor?',
-  //   question_type: {
-  //     name: 'Images_Inventor',
-  //     query: `SELECT ?templateLabel ?answerLabel
-  //     WHERE {
-  //       ?answer wdt:P31 wd:Q5;  # Instance of human
-  //               wdt:P106 wd:Q937857;  # Occupation: "inventor"
-  //               wdt:P18 ?template;
-  //       SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-  //     }
-  //     LIMIT 5  
-  //     `,
-  //     entities: [],
-  //     typeName: 'science',
-  //   },
-  // });
+  // Image Question templates
+
+  // Flag of a country, autonomous community of Spain or USA state
+  addQuestionTemplate({
+    questionTemplate: 'This flag is from...?',
+    question_type: {
+      name: 'Images_Flags',
+      query: `SELECT ?templateLabel ?answerLabel
+      WHERE {
+        ?answer wdt:P31 wd:$$$; # Entity
+                wdt:P41 ?template.  # Capital
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en"}
+      }
+      ORDER BY UUID() # Add randomness to the results
+      LIMIT 5
+      `,
+      entities: [
+        'Q6256', // Country (any)
+        'Q10742', // Autonomous Community of Spain
+        'Q35657' // State of the United States],
+      ],
+      typeName: 'geography',
+    },
+  });
+
+
+  // Guess the person by an image
+  // In this case physiscists
+  addQuestionTemplate({
+    questionTemplate: 'Who is this physiscist?',
+    question_type: {
+      name: 'Images_Physics',
+      query: `SELECT ?templateLabel ?answerLabel
+      WHERE {
+        ?answer wdt:P31 wd:Q5;  # Instance of human
+                wdt:P106 wd:Q169470;  # Occupation: "physicist"
+                wdt:P18 ?template;
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+      }
+      LIMIT 5    
+      `,
+      entities: [],
+      typeName: 'science',
+    },
+  });
+
+  // Guess the person by an image
+  // In this case inventors
+  addQuestionTemplate({
+    questionTemplate: 'Who is this inventor?',
+    question_type: {
+      name: 'Images_Inventor',
+      query: `SELECT ?templateLabel ?answerLabel
+      WHERE {
+        ?answer wdt:P31 wd:Q5;  # Instance of human
+                wdt:P106 wd:Q937857;  # Occupation: "inventor"
+                wdt:P18 ?template;
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+      }
+      LIMIT 5  
+      `,
+      entities: [],
+      typeName: 'science',
+    },
+  });
 
   addQuestionTemplate({
     questionTemplate: 'What is the official language of $$$?',
@@ -259,6 +259,46 @@ const generateSampleTest = () => {
       `,
       entities: [],
       typeName: 'geography',
+    },
+  });
+
+  addQuestionTemplate({
+    questionTemplate: 'Where did the Olympic Games of $$$ take place?',
+    question_type: {
+      name: 'Olympics',
+      query: `SELECT ?templateLabel ?answerLabel
+      WHERE {
+        ?olympicGame wdt:P31 wd:Q159821; # Instances of Olympic Games
+                     wdt:P276 ?answer;
+                     wdt:P585 ?date.
+        BIND(YEAR(?date) AS ?templateLabel)
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+      }
+      ORDER BY UUID()
+      LIMIT 5      
+      `,
+      entities: [],
+      typeName: 'sports',
+    },
+  });
+
+  addQuestionTemplate({
+    questionTemplate: 'Where did the Winter Olympic Games of $$$ take place?',
+    question_type: {
+      name: 'Olympics',
+      query: `SELECT ?templateLabel ?answerLabel
+      WHERE {
+        ?olympicGame wdt:P31 wd:Q82414; # Instances of Winter Olympic Games
+                     wdt:P276 ?answer;
+                     wdt:P585 ?date.
+        BIND(YEAR(?date) AS ?templateLabel)
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+      }
+      ORDER BY UUID()
+      LIMIT 5      
+      `,
+      entities: [],
+      typeName: 'sports',
     },
   });
 };

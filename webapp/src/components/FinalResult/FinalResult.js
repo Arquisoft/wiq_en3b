91165434f9cea6b5bd591300cd5f566a79fef93f
didierrc <didieryamil02@gmail.com>
2024-04-22 18:28:27 +0200

    Added default share text and some other params to X url. Added i18n for them.

--- webapp/src/components/FinalResult/FinalResult.js
+++ webapp/src/components/FinalResult/FinalResult.js
@@ -6,7 +6,13 @@ import { useTranslation } from 'react-i18next'
 
 const FinalResult = ({ result, quizLength, points, onPlayAgain, goBack }) => {
   const { t } = useTranslation()
+  const tweetUrl = "https://twitter.com/intent/tweet"
   
+  // https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/overview
+  let tweetToShare = tweetUrl + "?text="  + t("play.result.tweetShare",{correct: result, total: quizLength, points: points})
+  tweetToShare += "&url=https://www.kawgame.xyz%0A"
+  tweetToShare += "&hashtags=kaw,wikidata,know,win,game" 
+
   return (
     <div className="final-result">
       <h1>{t("play.result.title")}</h1>
@@ -16,7 +22,7 @@ const FinalResult = ({ result, quizLength, points, onPlayAgain, goBack }) => {
       <p>{t("play.result.points", { points: points })}</p>
       <Button onClick={onPlayAgain}>{t("play.result.play_again")}</Button>
 
-      <XButton href="https://twitter.com/intent/tweet"></XButton>
+      <XButton href={tweetToShare}></XButton>
 
       <Button onClick={goBack} className="danger">
         {t("play.result.go_back_button")}

c6aea32645d3dcbdb996daad27e2162a969394e2
krovostcora <AnnnaKutova@gmail.com>
2024-04-27 03:29:03 +0200

    trying to fix e2e test error

--- webapp/src/pages/AppLayout.js
+++ webapp/src/pages/AppLayout.js
@@ -1,42 +1,17 @@
-import {Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Header from '../components/Header/Header'
 import Nav from '../components/Nav/Nav'
-import {useTheme} from '../hooks/useTheme'
-import {useToggle} from '../hooks/useToggle'
-// import './cursor.css'
-import Lottie from 'react-lottie'
-import darkAnimationData from '../assets/catanimation/sleeping_cat.json';
-import lightAnimationData from '../assets/catanimation/asleep_cat.json';
+import { useTheme } from '../hooks/useTheme'
+import { useToggle } from '../hooks/useToggle'
 
-
-
-
-const AppLayout = ({volume}) => {
-    const darkOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: darkAnimationData,
-        rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice'
-        }
-    };
-
-    const lightOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: lightAnimationData,
-        rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice'
-        }
-    };
+const AppLayout = ({ volume }) => {
     //State for opening and closing the navigation
-    const [nav, toggleNav] = useToggle(false);
+    const [nav, toggleNav] = useToggle(false)
     //State for the theme
-    const {theme, changeThemeHandler} = useTheme();
+    const { theme, changeThemeHandler } = useTheme()
 
     return (
         <div className={theme}>
-
 
             <Header
                 onToggleNav={toggleNav}
@@ -44,18 +19,12 @@ const AppLayout = ({volume}) => {
                 theme={theme}
                 volume={volume}
             />
-            <Nav openNav={nav} onToggleNav={toggleNav}/>
+
+            <Nav openNav={nav} onToggleNav={toggleNav} />
+
             <main>
-                <Outlet/>
-                {theme === "dark" && (
-                    <Lottie options={darkOptions} height={180} width={280} style={{ position: 'absolute', bottom: 20, right: 80 }} />
-                )}
-                {theme === "light" && (
-                    <Lottie options={lightOptions} height={200} width={400} style={{ position: 'absolute', bottom: 12, right: 40 }} />
-                )}
-
+                <Outlet />
             </main>
-
         </div>
     )
 }

917537c98152c8537ea53f0beabeae1d4e8bdf0e
krovostcora <AnnnaKutova@gmail.com>
2024-04-27 03:20:49 +0200

    trying to fix e2e test error

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -9,7 +9,7 @@ import Settings from './pages/Settings/Settings'
 import Login from './pages/Login/Login'
 import Logout from './pages/Logout/Logout'
 import Register from './pages/Register/Register'
-// import PageNotFound from './pages/PageNotFound/PageNotFound';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
 
 import { ProtectedRoute } from './components/ProtectedRoute'
 
@@ -51,8 +51,8 @@ function App() {
               <Route path="profile/:username?" element={<Profile />}></Route>
               <Route path="leaderboard" element={<Leaderboard />}></Route>
               <Route path="settings" element={<Settings volume={volume} handleVolumeChange={handleVolumeChange} />}></Route>
+                <Route path="*" element={<PageNotFound />}></Route>
             </Route>
-            {/*<Route path="*" element={<PageNotFound />}></Route>*/}
           </Routes>
         </AuthProvider>
       </SettingsProvider>
--- webapp/src/pages/AppLayout.js
+++ webapp/src/pages/AppLayout.js
@@ -1,12 +1,12 @@
-import {Outlet} from 'react-router-dom'
+// import {Outlet} from 'react-router-dom'
 import Header from '../components/Header/Header'
 import Nav from '../components/Nav/Nav'
 import {useTheme} from '../hooks/useTheme'
 import {useToggle} from '../hooks/useToggle'
 import './cursor.css'
-import Lottie from 'react-lottie'
-import darkAnimationData from '../assets/catanimation/sleeping_cat.json';
-import lightAnimationData from '../assets/catanimation/asleep_cat.json';
+// import Lottie from 'react-lottie'
+// import darkAnimationData from '../assets/catanimation/sleeping_cat.json';
+// import lightAnimationData from '../assets/catanimation/asleep_cat.json';
 
 
 
@@ -46,13 +46,13 @@ const AppLayout = ({volume}) => {
             />
             <Nav openNav={nav} onToggleNav={toggleNav}/>
             <main>
-                <Outlet/>
-                {theme === "dark" && (
-                    <Lottie options={darkOptions} height={180} width={280} style={{ position: 'absolute', bottom: 20, right: 80 }} />
-                )}
-                {theme === "light" && (
-                    <Lottie options={lightOptions} height={200} width={400} style={{ position: 'absolute', bottom: 12, right: 40 }} />
-                )}
+                {/*<Outlet/>*/}
+                {/*{theme === "dark" && (*/}
+                {/*    <Lottie options={darkOptions} height={180} width={280} style={{ position: 'absolute', bottom: 20, right: 80 }} />*/}
+                {/*)}*/}
+                {/*{theme === "light" && (*/}
+                {/*    <Lottie options={lightOptions} height={200} width={400} style={{ position: 'absolute', bottom: 12, right: 40 }} />*/}
+                {/*)}*/}
 
             </main>
 

9a41d3f03991e07f42744d32d907dff808af3868
krovostcora <AnnnaKutova@gmail.com>
2024-04-27 03:58:36 +0200

    trying to fix e2e test error

--- webapp/src/pages/AppLayout.js
+++ webapp/src/pages/AppLayout.js
@@ -4,12 +4,33 @@ import Nav from '../components/Nav/Nav'
 import { useTheme } from '../hooks/useTheme'
 import { useToggle } from '../hooks/useToggle'
 import './cursor.css'
+import Lottie from 'react-lottie'
+import darkAnimationData from '../assets/catanimation/sleeping_cat.json';
+import lightAnimationData from '../assets/catanimation/asleep_cat.json';
 
 const AppLayout = ({ volume }) => {
     //State for opening and closing the navigation
     const [nav, toggleNav] = useToggle(false)
     //State for the theme
     const { theme, changeThemeHandler } = useTheme()
+
+    const darkOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: darkAnimationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    };
+
+    const lightOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: lightAnimationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    };
 
     return (
         <div className={theme}>
@@ -24,7 +45,13 @@ const AppLayout = ({ volume }) => {
             <Nav openNav={nav} onToggleNav={toggleNav} />
 
             <main>
-                <Outlet />
+                <Outlet/>
+                {theme === "dark" && (
+                    <Lottie options={darkOptions} height={180} width={280} style={{ position: 'absolute', bottom: 20, right: 80 }} />
+                )}
+                {theme === "light" && (
+                    <Lottie options={lightOptions} height={200} width={400} style={{ position: 'absolute', bottom: 12, right: 40 }} />
+                )}
             </main>
         </div>
     )

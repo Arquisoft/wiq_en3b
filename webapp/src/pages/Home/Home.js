9e08fcd30086c586607df779c66e48b1cf8a4a74
Murias10 <uo290009@uniovi.es>
2024-04-29 01:35:59 +0200

    Add play button to hame page

--- webapp/src/pages/Home/Home.js
+++ webapp/src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useAuth } from '../../hooks/useAuth'
 import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './Home.css'
 
 import { useTranslation } from 'react-i18next'
@@ -29,6 +30,19 @@ function Home() {
       ) : (
         <div>Please, login to play the game</div>
       )}
+
+      <Link to="game" class="btn">
+        <div class="swipe">{t("play.nav_title")}
+          <span class="container">
+            <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 0h24v24H0z" fill="none">
+              </path>
+              <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor">
+              </path>
+            </svg>
+          </span>
+        </div>
+      </Link>
     </div>
   )
 }

705826e57145184b5e91cc5ea7c7862256901a74
krovostcora <AnnnaKutova@gmail.com>
2024-04-28 17:26:28 +0200

    added tests for PageNotFound.js

--- webapp/src/pages/PageNotFound/PageNotFound.test.js
+++ webapp/src/pages/PageNotFound/PageNotFound.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import PageNotFound from './pageNotFound';
+import PageNotFound from './PageNotFound';
 import '@testing-library/jest-dom/extend-expect';
 
 jest.mock('react-lottie');

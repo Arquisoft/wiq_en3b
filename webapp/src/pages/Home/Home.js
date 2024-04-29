d322f11c4b58803d3e010d1a975e9020e0de1450
Murias10 <uo290009@uniovi.es>
2024-04-29 17:25:02 +0200

    Fix bugs error page

--- webapp/src/pages/Home/Home.js
+++ webapp/src/pages/Home/Home.js
@@ -33,7 +33,7 @@ function Home() {
 
       <Link to="game" class="btn">
         <div class="swipe">{t("play.nav_title")}
-          <span class="container">
+          <span class="startBtn">
             <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M0 0h24v24H0z" fill="none">
               </path>

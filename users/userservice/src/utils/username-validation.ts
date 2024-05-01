710784815783b57bb2fbf4f874587c029217afd4
Iyan Robles <UO288780@uniovi.es>
2024-05-01 15:50:31 +0200

    Removed unnecessary check in username validator

--- users/userservice/src/utils/username-validation.ts
+++ users/userservice/src/utils/username-validation.ts
@@ -2,7 +2,7 @@ import { Request } from 'express';
 
 function validateNoSpaces(req: Request, fieldsThatCannotHaveSpace: string[]) {
     for (const field of fieldsThatCannotHaveSpace) {
-        if (req.body[field]?.indexOf(' ') >= 0) {
+        if (req.body[field]!.indexOf(' ') >= 0) {
             throw new Error(`The field "${field}" cannot have spaces`);
         }
     }
